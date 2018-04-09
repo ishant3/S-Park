0ce2246af33cf9eb742c8b6d87343df923c9cc14
ishant3 <upadhyay.ishant@yahoo.com>
2018-04-09 14:28:13 +0200

    Added AppCompat reference and image files

--- S-Park_Android/MainActivity.cs
+++ S-Park_Android/MainActivity.cs
@@ -1,11 +1,12 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V7.App;
 
 namespace S_Park_Android
 {
-    [Activity(Label = "S_Park_Android", MainLauncher = true)]
-    public class MainActivity : Activity
+    [Activity(Label = "S-Park", MainLauncher = true, Theme = "@style/Theme.AppCompat.Light.NoActionBar",Icon ="@drawable/S_Park_Image")]
+    public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
