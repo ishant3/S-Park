a340b395c9febc2a7bd27742df5d2d9b7caf5cdc
ishant3 <upadhyay.ishant@yahoo.com>
2018-04-09 16:23:40 +0200

    After Sign Up Dialog Box

--- S-Park_Android/WelcomeActivity.cs
+++ S-Park_Android/WelcomeActivity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Threading;
 using Android.App;
 using Android.Content;
 using Android.OS;
@@ -16,12 +16,46 @@ namespace S_Park_Android
     [Activity(Label = "WelcomeActivity", Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
     public class WelcomeActivity: AppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+
+        private Button mBtnSignUp;
+        private ProgressBar mProgressBar;
+
+        protected override void OnCreate(Bundle bundle)
         {
-            base.OnCreate(savedInstanceState);
+            base.OnCreate(bundle);
 
             // Create your application here
             SetContentView(Resource.Layout.Main);
+
+            mBtnSignUp = FindViewById<Button>(Resource.Id.btnSignUp);
+            mProgressBar = FindViewById<ProgressBar>(Resource.Id.progressBar1);
+
+            mBtnSignUp.Click += (object sender, EventArgs args) =>
+            {
+                //Pull up dialog
+                FragmentTransaction transaction = FragmentManager.BeginTransaction();
+                dialog_SignUp signUpDialog = new dialog_SignUp();
+                signUpDialog.Show(transaction, "dialog fragment");
+
+                signUpDialog.mOnSignUpComplete += signUpDialog_mOnSignUpComplete;
+            };
+        }
+        void signUpDialog_mOnSignUpComplete(object sender, OnSignUpEventArgs e)
+        {
+
+            mProgressBar.Visibility = ViewStates.Visible;
+            Thread thread = new Thread(ActLikeARequest);
+            thread.Start();
+
+
+        }
+
+        private void ActLikeARequest()
+        {
+            Thread.Sleep(3000);
+
+            RunOnUiThread(() => { mProgressBar.Visibility = ViewStates.Invisible; });
+            int x = Resource.Animation.slide_right;
         }
     }
 }
