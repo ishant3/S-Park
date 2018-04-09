7f275f68580d555f7a6684f1d346c18a523bd6d3
ishant3 <upadhyay.ishant@yahoo.com>
2018-04-09 12:33:29 +0200

    Add project files.

---
+++ S-Park_Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace S_Park_Android
+{
+    [Activity(Label = "S_Park_Android", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
